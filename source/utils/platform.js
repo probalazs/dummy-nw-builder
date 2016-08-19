fc2602299364bfb4f36cb733f8a86ad083ee61e9
probalazs <probalazs@nyenyere.eu>
2016-08-19 14:29:33 +0200

    *NEW platform util created

---
+++ source/utils/platform.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const PLATFORMS = require('../config/config.constants').VALID_VALUES.platform;
+
+module.exports = {
+	isLinux: isPlatform.bind(null, PLATFORMS.linux),
+	isWindows: isPlatform.bind(null, PLATFORMS.windows),
+	isOsx: isPlatform.bind(null, PLATFORMS.osx)
+};
+
+function isPlatform(required, toBeTested) {
+	return required === toBeTested;
+}
+
