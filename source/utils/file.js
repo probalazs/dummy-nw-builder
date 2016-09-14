8c926ee169176e2c0b8ee427ff26803fbb8bdd63
probalazs <probalazs@nyenyere.eu>
2016-09-14 12:53:22 +0200

    *FIX eslint in sources/utils folder

--- source/utils/file.js
+++ source/utils/file.js
@@ -6,49 +6,49 @@ const fs = require('fs');
 const HTTP_STATUS_OK = 200;
 
 module.exports = {
-	createFolderSync: createFolderSync,
-	downloadFileFromUrl: downloadFileFromUrl
+    createFolderSync: createFolderSync,
+    downloadFileFromUrl: downloadFileFromUrl
 };
 
 function createFolderSync(folder) {
-	if (!_isFolderExist(folder)) {
-		fs.mkdirSync(folder);
-	}
+    if (!_isFolderExist(folder)) {
+        fs.mkdirSync(folder);
+    }
 }
 
 function downloadFileFromUrl(url, destination) {
-	let destinationStream = fs.createWriteStream(destination);
-	return new Promise(function(resolve, reject) {
-		https.get(url, (response) => {
-				if (_isOkStatus(response)) {
-					let streams = {
-						destination: destinationStream,
-						response: response
-					};
-					_copyFileToDestination(streams, resolve);
-				} else {
-					reject();
-				}
-			});
-	});
+    let destinationStream = fs.createWriteStream(destination);
+    return new Promise(function(resolve, reject) {
+        https.get(url, (response) => {
+            if (_isOkStatus(response)) {
+                let streams = {
+                    destination: destinationStream,
+                    response: response
+                };
+                _copyFileToDestination(streams, resolve);
+            } else {
+                reject();
+            }
+        });
+    });
 }
 
 function _copyFileToDestination(streams, ready) {
-	streams.response.pipe(streams.destination);
-	streams.destination.on('finish', () => {
-		streams.destination.close(ready);
-	});
+    streams.response.pipe(streams.destination);
+    streams.destination.on('finish', () => {
+        streams.destination.close(ready);
+    });
 }
 
 function _isOkStatus(response) {
-	return response.statusCode === HTTP_STATUS_OK;
+    return response.statusCode === HTTP_STATUS_OK;
 }
 
 function _isFolderExist(folder) {
-	try {
-		let stat = fs.statSync(folder);
-		return stat.isDirectory();
-	} catch (e) {
-		return false;
-	}
+    try {
+        let stat = fs.statSync(folder);
+        return stat.isDirectory();
+    } catch (e) {
+        return false;
+    }
 }
--- source/utils/platform.js
+++ source/utils/platform.js
@@ -3,12 +3,12 @@
 const PLATFORMS = require('../config/config.constants').VALID_VALUES.platform;
 
 module.exports = {
-	isLinux: isPlatform.bind(null, PLATFORMS.linux),
-	isWindows: isPlatform.bind(null, PLATFORMS.windows),
-	isOsx: isPlatform.bind(null, PLATFORMS.osx)
+    isLinux: isPlatform.bind(null, PLATFORMS.linux),
+    isWindows: isPlatform.bind(null, PLATFORMS.windows),
+    isOsx: isPlatform.bind(null, PLATFORMS.osx)
 };
 
 function isPlatform(required, toBeTested) {
-	return required === toBeTested;
+    return required === toBeTested;
 }
 
