02bfdd1509411450fcacedcb17b78fd4d91b6ea9
probalazs <probalazs@nyenyere.eu>
2016-09-14 12:55:33 +0200

    *FIX eslint sources/nw.downloader

--- source/nw.downloader.js
+++ source/nw.downloader.js
@@ -4,20 +4,19 @@ const path = require('path');
 const file = require('./utils/file');
 
 module.exports = class NwDownloader {
-	constructor(config) {
-		this._config = config;	
-		Object.freeze(this._config);
-	}
+    constructor(config) {
+        this._config = config;
+    }
 
-	download() {
-		let destination = this._getDestination;
-		return file.downloadFileFromUrl(this._config.nw.url, destination);
-	}
+    download() {
+        let destination = this._getDestination;
+        return file.downloadFileFromUrl(this._config.nw.url, destination);
+    }
 
-	_getDestination() {
-		let config = this._config;
-		let destination = path.join(config.folders.tmp, config.nw.file);
-		file.createFolderSync(this._config.folders.tmp);
-		return destination;
-	}
+    _getDestination() {
+        let config = this._config;
+        let destination = path.join(config.folders.tmp, config.nw.file);
+        file.createFolderSync(this._config.folders.tmp);
+        return destination;
+    }
 };
