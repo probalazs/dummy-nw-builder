e1bb8ba4f2b24bee0dd53a6615eb224376a4805a
probalazs <probalazs@nyenyere.eu>
2016-09-20 12:12:35 +0200

    *MOD file download simplify

--- source/utils/file.js
+++ source/utils/file.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const https = require('https');
+const Promise = require('bluebird');
+const request = require('request');
 const fs = require('fs');
-
-const HTTP_STATUS_OK = 200;
 
 module.exports = {
     createFolderSync: createFolderSync,
-    downloadFileFromUrl: downloadFileFromUrl
+    download: download
 };
 
 function createFolderSync(folder) {
@@ -16,32 +15,13 @@ function createFolderSync(folder) {
     }
 }
 
-function downloadFileFromUrl(url, destination) {
-    let destinationStream = fs.createWriteStream(destination);
-    return new Promise(function(resolve, reject) {
-        https.get(url, (response) => {
-            if (_isOkStatus(response)) {
-                let streams = {
-                    destination: destinationStream,
-                    response: response
-                };
-                _copyFileToDestination(streams, resolve);
-            } else {
-                reject();
-            }
-        });
+function download(url, destination) {
+    return new Promise((resolve, reject) => {
+        request.get(url)
+            .on('error', reject)
+            .pipe(fs.createWriteStream(destination))
+            .on('finish', resolve);
     });
-}
-
-function _copyFileToDestination(streams, ready) {
-    streams.response.pipe(streams.destination);
-    streams.destination.on('finish', () => {
-        streams.destination.close(ready);
-    });
-}
-
-function _isOkStatus(response) {
-    return response.statusCode === HTTP_STATUS_OK;
 }
 
 function _isFolderExist(folder) {
